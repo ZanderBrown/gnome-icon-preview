b89e12dbffee5fb7a8b2d1d73cd0ee5e55ab10f6
Alexander Mikhaylenko <alexm@gnome.org>
2021-10-16 02:18:00 +0500

    recents: Polish the popover
    Use the default combo style, remove the minimum height. Clean up the code.

--- src/widgets/recents/item.rs
+++ src/widgets/recents/item.rs
@@ -28,8 +28,6 @@ impl RecentItemRow {
     fn init(&self) {
         let container = gtk::Box::new(gtk::Orientation::Horizontal, 12);
         container.set_height_request(50);
-        container.set_margin_start(12);
-        container.set_margin_end(12);
 
         if let Ok((hicolor, _)) = self.project.get_hicolor(None) {
             let image = gtk::Image::from_gicon(&gio::FileIcon::new(&hicolor));
@@ -40,8 +38,7 @@ impl RecentItemRow {
         let project_name = self.project.name();
 
         let item_label = gtk::Label::new(Some(&project_name));
-        item_label.set_halign(gtk::Align::Start);
-        item_label.set_valign(gtk::Align::Center);
+        item_label.set_xalign(0.0);
         item_label.set_ellipsize(pango::EllipsizeMode::End);
         item_label.set_tooltip_text(Some(&project_name));
         item_label.add_css_class("recent-item");
